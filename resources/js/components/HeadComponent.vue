683f53f466d34075f165bd5f28133b8bb3ac6b72
Victor Morales <victor.morales@fusiona.cl>
2021-09-28 16:55:26 -0300

    Inicio

---
+++ resources/js/components/HeadComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>Hola Mundo Cabecera</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
